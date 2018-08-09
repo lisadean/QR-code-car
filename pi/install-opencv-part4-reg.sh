3e2c7d38854dbbb00f28eceaa56a59badbf63256
Lisa Dean <lisa@lisadean.net>
2018-08-09 11:56:38 -0400

    Install scripts for OpenCV on R Pi

---
+++ pi/install-opencv-part4-reg.sh
@@ -0,0 +1,13 @@
+# Run as regular user
+set -x
+
+
+pip install numpy
+cd ~/opencv-3.4.2/
+$ mkdir build
+$ cd build
+$ cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules \
+    -D BUILD_EXAMPLES=ON ..
